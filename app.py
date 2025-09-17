0c343e007ae1ce6c072a104939ee4d12926e2b9c
gponciano <gab_bash@hotmail.com>
2025-09-17 19:14:26 +1000

    Adding confirmed page styling and render template page

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def pix_confirmation():
     
     payment.paid = True
     db.session.commit()
-
+    socketio.emit(f"payment-confirmed-{payment.id}")
     return jsonify({"message": "The payment has been confirmed"})
 
 
@@ -71,7 +71,9 @@ def pix_confirmation():
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
 
-
+    if payment.paid:
+        return render_template("confirmed_payment.html",
+                               )
     return render_template("payment.html", 
                            payment_id=payment.id, 
                            value=payment.value, 
